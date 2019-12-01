0aaa88ac293f7908cf6c9841cea52de5067da7c3
Andrew <kitan012@mail.ru>
2019-12-01 19:30:51 +0300

    Initial commit

---
+++ Character Manager/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterManager
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Gearscore { get; set; }
+        public string Race { get; set; }
+        public string Class { get; set; }
+        public Character() { }
+
+    }
+}
