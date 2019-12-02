91ef3f6e34193b00ec26d00ed3a5c36c27322cf9
Andrew <kitan012@mail.ru>
2019-12-02 21:41:27 +0300

    Добавлен класс, описывающий личный аллод и рог изобилия

---
+++ Character Manager/Cornucopia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterManager
+{
+    public class Cornucopia
+    {
+        public int Id { get; set; }
+        public DateTime StartOfProduction { get; set; }
+        public int AmountDaysOfProduction { get; set; }
+        public string Product { get; set; }
+    }
+}
