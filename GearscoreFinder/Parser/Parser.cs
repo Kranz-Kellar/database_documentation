3870e31828df450dda346672b96e6a08816a16a3
Andrew <kitan012@mail.ru>
2019-12-07 19:38:04 +0300

    Добавлен проект GearscoreFinder.

---
+++ GearscoreFinder/Parser/Parser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace CharacterManager
+{
+    public class Parser
+    {
+        static private string allodsArmoryAddress = $"http://armory.allodswiki.ru";
+
+        static public void Init()
+        {
+
+        }
+
+        static public string GetGearScoreOfPlayer(string characterName, string serverName)
+        {
+            string translitServer = TransliterationHandler.RussianToEnglish(serverName.ToLower().Trim());
+            string translitName = TransliterationHandler.RussianToEnglish(characterName.ToLower().Trim());
+            var pageOfArmoryAllods = LoadPage($"{allodsArmoryAddress}/avatar/{translitServer}/{translitName}".ToLower());
+            var document = new HtmlDocument();
+            document.LoadHtml(pageOfArmoryAllods);
+
+            HtmlNodeCollection links = document.DocumentNode.SelectNodes(".//div[@class='font-weight-bold avatar-gearscore']");
+            if(links == null)
+            {
+                return "Гирскор не найден";
+            }
+            return links[0].InnerText;
+        }
+
+        static private string LoadPage(string url)
+        {
+            var result = "";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var receiveStream = response.GetResponseStream();
+                if (receiveStream != null)
+                {
+                    StreamReader readStream;
+                    if (response.CharacterSet == null)
+                        readStream = new StreamReader(receiveStream);
+                    else
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    result = readStream.ReadToEnd();
+                    readStream.Close();
+                }
+                response.Close();
+            }
+            return result;
+        }
+
+        static public string FindElement(string XElement)
+        {
+            return "";
+        }
+    }
+}
---
+++ GearscoreFinder/TransliterationHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterManager
+{
+    public class TransliterationHandler
+    {
+        static private Dictionary<string, string> transliterationDict;
+        static public void Init()
+        {
+            transliterationDict = new Dictionary<string, string>();
+            transliterationDict.Add("а", "A");
+            transliterationDict.Add("б", "B");
+            transliterationDict.Add("в", "V");
+            transliterationDict.Add("г", "G");
+            transliterationDict.Add("д", "D");
+            transliterationDict.Add("е", "E");
+            transliterationDict.Add("ё", "E");
+            transliterationDict.Add("ж", "ZH");
+            transliterationDict.Add("з", "Z");
+            transliterationDict.Add("и", "I");
+            transliterationDict.Add("й", "Y");
+            transliterationDict.Add("к", "K");
+            transliterationDict.Add("л", "L");
+            transliterationDict.Add("м", "M");
+            transliterationDict.Add("н", "N");
+            transliterationDict.Add("о", "O");
+            transliterationDict.Add("п", "P");
+            transliterationDict.Add("р", "R");
+            transliterationDict.Add("с", "S");
+            transliterationDict.Add("т", "T");
+            transliterationDict.Add("у", "U");
+            transliterationDict.Add("ф", "F");
+            transliterationDict.Add("х", "KH");
+            transliterationDict.Add("ц", "CE");
+            transliterationDict.Add("ч", "CH");
+            transliterationDict.Add("ш", "SH");
+            transliterationDict.Add("щ", "SHCH");
+            transliterationDict.Add("ъ", "_");
+            transliterationDict.Add("ы", "YI");
+            transliterationDict.Add("ь", "-");
+            transliterationDict.Add("э", "EH");
+            transliterationDict.Add("ю", "YU");
+            transliterationDict.Add("я", "YA");
+            transliterationDict.Add(" ", "-");
+        }
+
+        static public string RussianToEnglish(string word)
+        {
+            StringBuilder ret = new StringBuilder();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var wordChar = word.Substring(i, 1);
+                var character = transliterationDict[wordChar];
+                ret.Append(character);
+            }
+
+            return ret.ToString();
+        }
+    }
+}
